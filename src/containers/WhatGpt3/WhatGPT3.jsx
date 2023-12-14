a872744252f69fad1d5bd89bc6f10d850c9a465b
srihanth-m <srishanthmule@gmail.com>
2023-12-14 16:24:05 +0530

    Brand And TextFeature built

--- src/containers/WhatGpt3/WhatGPT3.jsx
+++ src/containers/WhatGpt3/WhatGPT3.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
 import './whatGPT3.css'
+import { Feature } from '../../components'
 
 const WhatGPT3 = () => {
   return (
-    <div>
-      
+    <div className='gpt3_whatgpt3 section_margin' id='whatgpt3'>
+      <div className="gpt3_whatgpt3-features">
+        <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."/>
+      </div>
+      <div className='gpt3_whatgpt3-heading'>
+        <div className="gradient_text heading">The possibilities are beyond your imagination</div>
+        <p className="gradient_text">Explore The Library</p>
+      </div>
+      <div className="gpt3_whatgpt3-container">
+        <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought."/>
+        <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"/>
+        <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments "/>
+      </div>
     </div>
   )
 }
 
 export default WhatGPT3
+
+// What is GPT-3
+// We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.
+// The possibilities are beyond your imagination
+// Explore The Library
+// Chatbots
+// Knowledgebase
+// Education
+// We so opinion friends me message as delight. Whole front do of plate heard oh ought. 
+// At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b
+// At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b
