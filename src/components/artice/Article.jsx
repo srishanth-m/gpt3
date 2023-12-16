edef1bad2bf01148d6f9945b9fd9d0d05391d235
srihanth-m <srishanthmule@gmail.com>
2023-12-16 10:10:42 +0530

    gpt-3 web app built completely

--- src/components/artice/Article.jsx
+++ src/components/artice/Article.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './article.css'
 
-const Article = () => {
+const Article = ({imgURL , date , title}) => {
   return (
-    <div>
-      
+    <div className='gpt3_blog-container_article'>
+      <div className="gpt3_blog-container_article-image">
+        <img src={imgURL} />
+      </div>
+      <div className="gpt3_blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read full article</p>
+      </div>
     </div>
   )
 }
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
-import './blog.css'
+import React from "react";
+import "./blog.css";
+import { Article } from "../../components";
+import {
+  blog01,
+  blog02,
+  blog03,
+  blog04,
+  blog05,
+} from "../../components/artice/imports";
 
 const Blog = () => {
   return (
-    <div>
-      blog
+    <div className="gpt3_blog section_padding" id="blog">
+      <div className="gpt3_blog-heading">
+        <h1 className="gradient_text">
+          A lot is happening <br></br> we are blogging about it
+        </h1>
+      </div>
+      <div className="gpt3_blog-container">
+        <div className="gpt3_blog-container_groupA">
+          <Article
+            imgURL={blog01}
+            date={"16-12-2023"}
+            title={"GPT-3 and Open AI is the future let us explore how it is"}
+          />
+        </div>
+        <div className="gpt3_blog-container_groupB">
+          <Article
+            imgURL={blog02}
+            date={"16-12-2023"}
+            title={"GPT-3 and Open AI is the future let us explore how it is"}
+          />
+          <Article
+            imgURL={blog03}
+            date={"16-12-2023"}
+            title={"GPT-3 and Open AI is the future let us explore how it is"}
+          />
+          <Article
+            imgURL={blog04}
+            date={"16-12-2023"}
+            title={"GPT-3 and Open AI is the future let us explore how it is"}
+          />
+          <Article
+            imgURL={blog05}
+            date={"16-12-2023"}
+            title={"GPT-3 and Open AI is the future let us explore how it is"}
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
